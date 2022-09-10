a978d43a37fbe0a6a11dc9c1e92f523d8003b29e
AdyatmaNugrahaYudha <awan2403@gmail.com>
2022-09-10 23:14:15 +0700

    Do stuff...

--- katalog/views.py
+++ katalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
-from katalog.models import CatalogItem
 def show_katalog(request):
     data_barang_katalog = CatalogItem.objects.all()
     context = {
