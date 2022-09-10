172dccc802ab577838f112dc8a0819855d8904e1
AdyatmaNugrahaYudha <awan2403@gmail.com>
2022-09-10 21:59:22 +0700

    Mengaktifkan page katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # TODO: Create your views here.
+from katalog.models import CatalogItem
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Adyatma W.A.N.Y.',
+        'npm': '2106750805'
+    }
+    return render(request, "katalog.html", context)
