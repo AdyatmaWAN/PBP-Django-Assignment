475dc6d04877c4d277ea9d325801a44dd3b59a3b
AdyatmaNugrahaYudha <awan2403@gmail.com>
2022-11-24 00:39:22 +0700

    Float >:(

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class WatchListItem(models.Model):
     watched = models.BooleanField()
     title = models.CharField(max_length=255)
-    rating = models.FloatField()
+    rating = models.IntegerField()
     release_date = models.TextField()
     review = models.TextField()
 
